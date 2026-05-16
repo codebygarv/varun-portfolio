99a8be41572ba2a57a31f20ab84f22920cac7139
Garv <garvthakral90@gmail.com>
2026-05-16 12:47:44 +0530

    Remove the Unused declared Variables

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 import gsap from 'gsap';
